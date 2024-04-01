dc8ac40150a7be07349be4293e88f392e70893a8
dettyo <yu.rubysoho@gmail.com>
2024-04-01 11:20:41 +0900

    出品する投稿画面遷移

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -5,4 +5,26 @@ class FurimasController < ApplicationController
     #@furimas = Furima.new
   end
 
+  def new
+    @furima = Furima.new
+  end
+
+  def create
+    @furima = Furima.create(furima_params)
+  end
+
+  private
+  #def move_to_index
+  #  prototype = Prototype.find(params[:id])
+  #  #unless user_signed_in?
+  #  unless current_user.id == prototype.user_id
+  #    redirect_to action: :index
+  #  end
+  #end
+
+  def furima_params
+    params.require(:furima).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+
 end
