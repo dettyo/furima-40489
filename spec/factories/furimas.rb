de69185fbb5714be0cf41d896c587b510cfde651
dettyo <yu.rubysoho@gmail.com>
2024-04-02 13:15:58 +0900

    単体テストひな型作成

--- spec/factories/furimas.rb
+++ spec/factories/furimas.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :furima do
-    
+    item_name               {'test'}
+    explanation             {'test'}
+    category_id             {4}
+    condition_id            {4}
+    shipping_fee_info_id    {5}
+    prefecture_id           {6}
+    shipping_date_info_id   {7}
+    price                   {1000}
+    image { Rack::Test::UploadedFile.new('app/assets/images/comment.png', 'comment/png') } 
+    association :user
   end
 end
--- spec/models/furima_spec.rb
+++ spec/models/furima_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Furima, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @furima = FactoryBot.build(:furima)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録がうまくいく時' do
+      it '全ての項目が正しく入力されていれば保存できること' do
+        expect(@furima).to be_valid
+      end
+    end
+
+    context '新規登録がうまくいかない時' do
+      it '商品名が空だと保存できないこと' do
+        @furima.item_name = ''
+        @furima.valid?
+        expect(@furima.errors.full_messages).to include("Item name can't be blank")
+      end
+    end
+  end
 end
