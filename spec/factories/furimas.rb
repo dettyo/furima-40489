d5ae76fab5842ff44e707386d72f54a1ac140ce9
dettyo <yu.rubysoho@gmail.com>
2024-04-02 14:03:10 +0900

    単体テストコード作成

--- spec/factories/furimas.rb
+++ spec/factories/furimas.rb
@@ -2,13 +2,13 @@ FactoryBot.define do
   factory :furima do
     item_name               {'test'}
     explanation             {'test'}
-    category_id             {4}
-    condition_id            {4}
-    shipping_fee_info_id    {5}
-    prefecture_id           {6}
-    shipping_date_info_id   {7}
+    category_id             {2}
+    condition_id            {2}
+    shipping_fee_info_id    {2}
+    prefecture_id           {2}
+    shipping_date_info_id   {2}
     price                   {1000}
-    image { Rack::Test::UploadedFile.new('app/assets/images/comment.png', 'comment/png') } 
+    image                   { Rack::Test::UploadedFile.new('app/assets/images/comment.png', 'comment/png') } 
     association :user
   end
 end
--- spec/models/furima_spec.rb
+++ spec/models/furima_spec.rb
@@ -18,6 +18,73 @@ RSpec.describe Furima, type: :model do
         @furima.valid?
         expect(@furima.errors.full_messages).to include("Item name can't be blank")
       end
+
+      it '商品説明が空だと保存できないこと' do
+        @furima.explanation = ''
+        @furima.valid?
+        expect(@furima.errors.full_messages).to include("Explanation can't be blank")
+      end
+
+      it 'カテゴリーが空だと保存できないこと' do
+        @furima.category_id = nil
+        @furima.valid?
+        expect(@furima.errors.full_messages).to include("Category can't be blank")
+      end
+
+      it '状態が空だと保存できないこと' do
+        @furima.condition_id = nil
+        @furima.valid?
+        expect(@furima.errors.full_messages).to include("Condition can't be blank")
+      end
+      
+      it '発送費情報が空だと保存できないこと' do
+        @furima.shipping_fee_info_id = nil
+        @furima.valid?
+        expect(@furima.errors.full_messages).to include("Shipping fee info can't be blank")
+      end
+      
+      it '都道府県が空だと保存できないこと' do
+        @furima.prefecture_id = nil
+        @furima.valid?
+        expect(@furima.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      
+      it '発送日時情報が空だと保存できないこと' do
+        @furima.shipping_date_info_id = nil
+        @furima.valid?
+        expect(@furima.errors.full_messages).to include("Shipping date info can't be blank")
+      end
+      
+      it '価格が空だと保存できないこと' do
+        @furima.price = nil
+        @furima.valid?
+        expect(@furima.errors.full_messages).to include("Price can't be blank")
+      end
+      
+      it '画像が空だと保存できないこと' do
+        @furima.image = nil
+        @furima.valid?
+        expect(@furima.errors.full_messages).to include("Image can't be blank")
+      end
+
+      it '価格は300円以下だと保存できないこと' do
+        @furima.price = 299
+        @furima.valid?
+        expect(@furima.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+
+      it '価格は10000000円以上だと保存できないこと' do
+        @furima.price = 10000000
+        @furima.valid?
+        expect(@furima.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+
+      it '価格は半角数値のみ保存可能であること' do
+        @furima.price = '１０００'
+        @furima.valid?
+        expect(@furima.errors.full_messages).to include("Price is not a number")
+      end
+
     end
   end
 end
