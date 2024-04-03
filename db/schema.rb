24d00f255fcc464dda45fa98fbc0ff1cfe261a06
dettyo <yu.rubysoho@gmail.com>
2024-04-03 15:03:10 +0900

    orderモデル&テーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_02_004150) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_03_055805) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -69,6 +69,28 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_02_004150) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
+  create_table "order_infos", charset: "utf8", force: :cascade do |t|
+    t.string "post_code", null: false
+    t.integer "prefecture_id", null: false
+    t.string "city", null: false
+    t.string "address", null: false
+    t.string "building"
+    t.string "telephone_num", null: false
+    t.bigint "order_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_infos_on_order_id"
+  end
+
+  create_table "orders", charset: "utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_orders_on_item_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
@@ -91,4 +113,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_02_004150) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "furimas", "users"
   add_foreign_key "items", "users"
+  add_foreign_key "order_infos", "orders"
+  add_foreign_key "orders", "items"
+  add_foreign_key "orders", "users"
 end
